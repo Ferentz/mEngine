8185566a0a8e3580569d27f91a4940301fc2d0dd
rentz <fv_intheinn@icloud.com>
2026-06-03 17:41:44 +0200

    mixup of initialization order

--- Minigin/buttons/Button.cpp
+++ Minigin/buttons/Button.cpp
@@ -7,9 +7,9 @@ namespace dae
 {
 	Button::Button(GameObject& obj, std::string const& name, std::shared_ptr<Font> & font , SDL_Color const & col, SDL_Color const & selectCol, std::function<void()> func)
 		:GameComponent{obj}
-		, Execute{func}
 		, colour{col}
 		, selectColour{selectCol}
+		, Execute{ func }
 	{
 		text = GetGameObject()->AddNGetComponent<TextComponent>(name, font, col);
 	}
